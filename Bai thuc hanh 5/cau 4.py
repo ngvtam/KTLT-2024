973c6eb22e982fa833e6399ec8264db1ec7d1ce4
ngvtam <nguyenvantam101005@gmail.com>
2024-11-28 08:08:15 +0700

    Add files via upload

---
+++ Bai thuc hanh 5/cau 4.py
@@ -0,0 +1,5 @@
+print("Nguyen Van Tam","\nMSSV: 235752021610054")
+a=list(range(12,38))
+print('Mang goc: ',a)
+b=a[::-1]
+print('mang dao nguoc: ',b)
---
+++ Bai thuc hanh 5/cau 8.py
@@ -0,0 +1,4 @@
+print("Nguyen Van Tam","\nMSSV: 235752021610054")
+a=str(input("Nhap danh sach: ")).split()
+x=int(input("Nhap so can tim: "))
+
